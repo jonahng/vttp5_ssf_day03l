78aceaae2db5d0323c4d2e0522f6216bb89d169f
jonahng <nyhj1998@gmail.com>
2024-11-20 10:49:31 +0800

    spring template

---
+++ src/main/java/com/jonah/vttp5_ssf_day03l/Vttp5SsfDay03lApplication.java
@@ -0,0 +1,13 @@
+package com.jonah.vttp5_ssf_day03l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5SsfDay03lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5SsfDay03lApplication.class, args);
+	}
+
+}
